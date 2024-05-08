bbda92037aeccec017748316211639aa486e0406
cristhianf3193 <cristhianf3193@gmail.com>
2024-05-08 00:24:07 -0500

    Prueba

---
+++ script.js
@@ -0,0 +1,24 @@
+function buttonClick(buttonId) {
+    alert('Button ' + buttonId + ' clicked!');
+}
+const countdownDate = new Date("June 01, 2024 00:00:00").getTime();
+
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("timer").innerHTML = "<h2>¡Ha llegado el día!</h2>";
+    }
+}, 1000);
